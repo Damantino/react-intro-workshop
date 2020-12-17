cf6233df26615718f98f9406c4c634589a8139e0
Alfredo Narvaez <a.narvaez@assemblerschool.com>
2020-12-17 17:27:13 +0100

    feat: remove solution code

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,16 +1,3 @@
-import React from "react";
-import Card from "./Card";
-
 export default function CardList(props) {
-  return (
-    (props.list && (
-      <div>
-        {props.list.map((person) => (
-          <Card key={person.name}>
-            {person.name} {person.lastName}
-          </Card>
-        ))}
-      </div>
-    )) || <div>No users!</div>
-  );
+  // Create me! 💡
 }
