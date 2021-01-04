c89658097216121351b2b5188f36edbd4f2f8378
Alfredo Narvaez <alfredonado@gmail.com>
2021-01-04 11:28:35 +0100

    feat: enhance instructions

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,3 +1,14 @@
+// 1. Import react 💡
+
 export default function CardList(props) {
-  // Create me! 💡
+  /**
+   * 2.1 Create a new component that receives a `list` prop.
+   *    This list prop should be an array of Objects with the following shape:
+   *    { name: "John", lastName: "Doe" }
+   *
+   * 2.2 Iterate throught all the items in the list, and for each render a new Card component
+   *     From the previous iteration.
+   *
+   * 2.3 Add a condition that checks if the list is empty. If it is, render a 'No users!' text
+   */
 }
