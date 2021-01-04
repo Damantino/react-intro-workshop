243e6b5995d4cff10ddfb03611666b68c34d17cc
Alfredo Narvaez <alfredonado@gmail.com>
2021-01-04 11:19:38 +0100

    feat: enhance instructions

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,12 @@
-import React from "react";
-import './Card.css';
+// 1. Import react 💡
+import "./Card.css";
 
-export default function Card(props) {
-    // Implement me! 💡
+export default function Card() {
+  /* 2.1 Make use of the 'children' property inside of the 'prop' object
+        So that you can make this component accept other components/Nodes and render them inside.
+        For instance {props.children}
+
+     2.2 Make use of the 'card-wrapper' css class at the outermost component. 
+        @hint remember that classes in JSX are denoted by the className attribute   
+   */
 }
