992953f0c4802dd166148a4f84ef46f3350a40c2
Alfredo Narvaez <alfredonado@gmail.com>
2021-01-04 13:13:53 +0100

    feat: remove implementation

--- src/components/Greeter.js
+++ src/components/Greeter.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 function Greeter(props) {
-  const [name, setName] = React.useState(props.name);
-  const greetingText = `Hello ${name} ${props.emoji}`;
-  const farewellText = `Bye ${name} ${props.emoji}`;
+  // 1.1 Import / Use `useState` hook from react to create a local state based on the `prop.name`
+  // 1.2 Once you have access to the name in the state, use it below instead of props.name
+  const greetingText = `Hello ${props.name} ${props.emoji}`;
+  const farewellText = `Bye ${props.name} ${props.emoji}`;
 
   React.useEffect(() => {
     document.title = greetingText;
@@ -11,20 +12,21 @@ function Greeter(props) {
     return () => {
       document.title = farewellText;
     };
-  }, [name, props.emoji]);
+    // 2. Make sure to add the name state to the dependency array, as when it changes, we want
+    // The side effect to run.
+  }, [props.name, props.emoji]);
 
-  function handleChangeName(e) {
-    setName(e.target.value);
-  }
+  /**
+   * 3. Create a new function that calls the `setName` function with the new value of the input
+     @hint remember you can access the new value of the input from the event
+   */
 
   return (
     <div>
       <h1>{greetingText}</h1>
-      <input
-        type="text"
-        placeholder="Type your name here"
-        onChange={handleChangeName}
-      />
+      {/* 4. Create a new input and add a new onChange event handler to it. It should call the function
+        defined on step 2
+      */}
     </div>
   );
 }
