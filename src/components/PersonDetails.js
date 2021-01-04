55b3a69480a9eb954541b3f464443e2fed5c14cb
Alfredo Narvaez <alfredonado@gmail.com>
2021-01-04 11:14:05 +0100

    feat: enhance instructions

--- src/components/PersonDetails.js
+++ src/components/PersonDetails.js
@@ -1,5 +1,23 @@
-import React from "react";
+// 1. Import react 💡
 
-export default function PersonDetails(props) {
-  // Create me 💡
+export default function PersonDetails() {
+  /**
+      2. Create a new component so that it receives an `Object`
+      through props called `user` that follows this structure:
+      user = {
+        name: "John",
+        lastName: "Doe",
+        favoriteGenre: "EDM",
+      }
+      
+      2.1 You can then access user.name through `props` and render each field
+      In your component. For example: "favorite music genre: edm"
+
+      2.2 Add a new prop called `sayCatchPhrase` which receives a lambda / function that console.logs 
+      The user's chatch phrase
+
+      2.3 Create a button and add the sayCathPhrase function to the onClick handler of that button
+      
+      2.4 Add a 'say catch phrase' text to the button 
+   */
 }
