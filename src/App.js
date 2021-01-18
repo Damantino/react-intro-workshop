97c7bac31238911dcd8a729b856dd857e9b2510b
Alfredo Narvaez <a.narvaez@assemblerschool.com>
2021-01-18 18:00:28 +0100

    feat: remove implementation

--- src/App.js
+++ src/App.js
@@ -12,46 +12,29 @@ function App() {
   ];
 
   return (
+    /**
+     *   1. Wrap your current app inside a new Route, inside a Switch with a path of '/'
+     *   2. Add a new Route with a path of '/other'
+     *   3. Add navigation links making use of the <Link> component. Pass a "to" prop indicating the path to go to
+     *   4. Make sure to have navigation for your home and the '/other' route
+     *    @hint Keep in mind the order for the routes inside the switch. Your root route should be last.
+     * */
     <div>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/other">other</Link>
-              </li>
-            </ul>
-          </nav>
-
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/other">
-              <h1>Lorem Ipsum</h1>
-            </Route>
-            <Route path="/">
-              <Greeter name="Assembler" emoji="🖐" />
-              <PersonDetails
-                user={{
-                  name: "John",
-                  lastName: "Doe",
-                  favoriteGenre: "EDM",
-                }}
-                sayCatchPhrase={() => console.log("Howdy!")}
-              />
-              <Card>
-                <h1>Hi there!</h1>
-                <p>I am inside a card</p>
-                <Greeter name="Jane Doe" emoji="🎈" />
-              </Card>
-              <CardList list={peopleList} />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      <Greeter name="Assembler" emoji="🖐" />
+      <PersonDetails
+        user={{
+          name: "John",
+          lastName: "Doe",
+          favoriteGenre: "EDM",
+        }}
+        sayCatchPhrase={() => console.log("Howdy!")}
+      />
+      <Card>
+        <h1>Hi there!</h1>
+        <p>I am inside a card</p>
+        <Greeter name="Jane Doe" emoji="🎈" />
+      </Card>
+      <CardList list={peopleList} />
     </div>
   );
 }
